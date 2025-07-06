f5a20d37753d67297dea8e27a86f6bc2d5b5bd91
hamimbsf <hamim.bsf@gmail.com>
2025-07-06 16:04:55 +0600

    How to move all 0s to the left side of an array?

--- practice.js
+++ practice.js
@@ -131,3 +131,15 @@
 // Question Six --> How to move all 0s to the left side of an array?
 
 let arr = [0, 1, 0, 0, 1, 1, 1, 1, 0];
+let i = 0;
+let j = 0;
+while (i < arr.length) {
+  if (arr[i] === 0) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    j++;
+  }
+  i++;
+}
+console.log(arr);
